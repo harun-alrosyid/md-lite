1d07ee4ee05b4051a225c2b220c5c078f55ba05f
harun-alrosyid <alrosyid07@gmail.com>
2026-03-03 03:56:30 +0700

    test: update parser tests to track architectural movements

--- src/heading-parse.test.ts
+++ src/heading-parse.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { Editor } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
-import { CustomImage } from './lib/CustomImage';
-import { CustomLink } from './lib/CustomLink';
+import { CustomImage } from './lib/extensions/CustomImage';
+import { CustomLink } from './lib/extensions/CustomLink';
 import { Markdown } from 'tiptap-markdown';
 
 describe('Markdown Parsing', () => {
